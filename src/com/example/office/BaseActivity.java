7560318939e63e23e3a86cbac5bae703921ace86
misuchiru <peimintsai@220-137-62-179.dynamic.hinet.net>
2014-06-27 21:05:36 +0800

    officePracticev1

---
+++ src/com/example/office/BaseActivity.java
@@ -0,0 +1,29 @@
+package com.example.office;
+
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+public abstract class BaseActivity extends Activity{
+
+	Context CONTEXT;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);	
+		init();
+		setContentView(getLayoutId());
+	}
+	
+	public void init(){
+		CONTEXT = this;
+	}
+	
+	public abstract int getLayoutId();
+	public abstract void findViewById();
+	public abstract void setListener();
+	public abstract void setView();	
+	public abstract void doExtra();
+}
